038259b16654d837c904535b9796ba264af42ebf
Gabriel Cedraz <cedrazdev@gmail.com>
2024-01-14 19:00:35 -0300

    removido decorator ispublic do userconstroller

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -10,7 +10,6 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { IsPublic } from 'src/shared/decorators/isPublic';
 
 @Controller('user')
 export class UserController {
