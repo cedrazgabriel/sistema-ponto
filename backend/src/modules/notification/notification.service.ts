4a63d0c93a5d347a278bbf87447596a9fb1007bd
Gabriel Cedraz <cedrazdev@gmail.com>
2024-01-18 21:38:33 -0300

    feat: implementa mensagem de whatsapp

--- backend/src/modules/notification/notification.service.ts
+++ backend/src/modules/notification/notification.service.ts
@@ -1,20 +1,50 @@
 import { Injectable } from '@nestjs/common';
-import { Twilio } from 'twilio';
+import axios from 'axios';
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class NotificationService {
   async sendNotificationWhatsApp() {
-    const accountSid = 'inseriraqui';
-    const authToken = 'inseriraui';
+    const idInstancia = 'colocarIdInstancia';
+    const tokenInstancia = 'colocarTokenIntancia';
+    const clientToken = 'colocarClientToken';
+    const baseUrl = `https://api.z-api.io/instances/${idInstancia}/token/${tokenInstancia}/send-text`;
 
-    const client = new Twilio(accountSid, authToken);
+    const contatos = [
+      {
+        name: 'Gabriel Cedraz',
+        phone: '5571991747951',
+      },
+    ];
 
-    client.messages
-      .create({
-        from: 'whatsapp:+12062025536', // From a valid Twilio number
-        body: 'Hello from twilio-node',
-        to: 'whatsapp:+5571991747951', // Text your number
-      })
-      .then((message) => console.log(message.sid));
+    const arquivoPdf = readFileSync('meu/caminho/do/arquivo.pdf');
+
+    const arquivoBase64 = arquivoPdf.toString('base64');
+
+    for (const contato of contatos) {
+      const body = {
+        phone: contato.phone,
+        caption: 'É o último, prometo! kk',
+        fileName: 'testeEnvioPdf',
+        document: 'data:application/pdf;base64,' + arquivoBase64,
+        delayMessage: 10,
+      };
+
+      axios
+        .post(baseUrl, body, {
+          headers: {
+            'Client-Token': clientToken,
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((response) => {
+          // Lidere com a resposta da requisição
+          console.log(response.data);
+        })
+        .catch((error) => {
+          // Lidere com erros da requisição
+          console.error(error);
+        });
+    }
   }
 }
