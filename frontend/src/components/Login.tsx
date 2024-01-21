e5813e6bbcd55d13438746ac9d6eeee38b0f8c04
Gabriel Cedraz <cedrazdev@gmail.com>
2024-01-21 20:45:10 -0300

    criado estrutura da página de login

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,42 @@
+import { Label } from "@radix-ui/react-label";
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+
+export function Login(){
+    return (
+        <Card className="w-[500px]">
+            <CardHeader className="flex flex-col items-center gap-4 text-center">
+                <CardTitle>Bem vindo ao sistema de ponto</CardTitle>
+                <CardDescription>Informe os dados da sua conta registrada:</CardDescription>
+            </CardHeader>
+            <form>
+                <div className="p-4 flex flex-col gap-3">
+                    <div className="flex flex-col space-y-2">
+                        <Label htmlFor="username">Usuário</Label>
+                        <Input id="username" placeholder="Nome do seu usuário"></Input>
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                        <Label htmlFor="senha">Senha</Label>
+                        <Input id="senha" type="password" placeholder="Senha cadastrada"></Input>
+                    </div>
+                    <div>
+                    <Button className="w-full">
+                        Entrar
+                    </Button>
+                </div>
+                <div className="text-center">
+                <p className="space-x-2">
+                <span className ="tracking-[-0.5px]">
+                    Novo por aqui?
+                </span>
+                <a href="">Crie uma conta</a>
+               </p>
+                </div>
+                </div>
+                
+            </form>
+        </Card>
+    )
+}
