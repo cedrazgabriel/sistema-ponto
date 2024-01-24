c8d00da58d7daed3dbcd32a25930de005e1960fe
Gabriel Cedraz <cedrazdev@gmail.com>
2024-01-24 13:57:28 -0300

    add dockerfile

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
+import { JustificativaModule } from './modules/justificativa/justificativa.module';
 import { MotivoModule } from './modules/motivo/motivo.module';
 import { NotificationModule } from './modules/notification/notification.module';
 import { PerfilModule } from './modules/perfil/perfil.module';
 import { RegistroModule } from './modules/registro/registro.module';
 import { UserModule } from './modules/user/user.module';
 import { DatabaseModule } from './shared/database/database.module';
-import { JustificativaModule } from './modules/justificativa/justificativa.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     DatabaseModule,
     AuthModule,
